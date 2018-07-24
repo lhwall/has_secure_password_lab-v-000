6885f5048d4980e64acccfdc9e5acccb579ed0f1
Lucy Wall <lhwall@gmail.com>
2018-07-24 17:37:00 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
 
     return redirect_to(controller: 'sessions', action: 'new') if !params[:user][:name] || params[:user][:name].empty?
-session[:user_id] = User.find_by(name: params[:user][:name]).id
+@user = User.find_by(name: params[:user][:name]).id
+return head(:forbidden) unless @user.authenticate(params[:password])
+session[:user_id] = @user.id
 redirect_to controller: 'users', action: 'home'
   end
 
