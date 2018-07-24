20da6bb0810a987145ce66586394e0281dea5fbf
Lucy Wall <lhwall@gmail.com>
2018-07-24 17:35:07 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     byebug
     return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
-session[:user_id] = User.find_by(name: params[:name]).id
+session[:user_id] = User.find_by(name: params[:user][:name]).id
 redirect_to controller: 'users', action: 'home'
   end
 
